3e7afdcc7116d157d7e937c186c38bca7d8418b2
omora14 <owenmorales9@gmail.com>
2024-02-06 12:29:42 -0600

    Fix some minor issues

--- chamber/scripts/getdate.js
+++ chamber/scripts/getdate.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     var currentYear = new Date().getFullYear();
-    document.querySelector('footer p:first-child').innerHTML = '&copy; ' + currentYear + '<br>Owen Morales' + '<br>WDD230 Web Frontend Development' + '<br>info@example.com';
+    var footer = document.querySelector('footer');
 
+    footer.querySelector('p:first-child').innerHTML = '&copy; ' + currentYear + '<br>Owen Morales' + '<br>WDD230 Web Frontend Development';
+
+    var fakeInfo = document.createElement('p');
+    fakeInfo.innerHTML = 'info@example.com' + '<br>+1 278 343 4324' + '<br>St Street, NY, 19201';
+    fakeInfo.style.float = 'left';
+    fakeInfo.style.clear = 'both';
+    footer.appendChild(fakeInfo);
 
     var lastModifiedDate = document.lastModified;
-    document.getElementById('lastModified').innerText = 'Last Modified: ' + lastModifiedDate;
+    var lastModifiedElement = document.createElement('p');
+    lastModifiedElement.innerText = 'Last Modified: ' + lastModifiedDate;
+    footer.appendChild(lastModifiedElement);
 });
