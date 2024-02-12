5059b746e403b493adfb797ae617ab55cc4e98c8
omora14 <owenmorales9@gmail.com>
2024-02-12 11:32:30 -0600

    Added toggle button

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,9 +1,11 @@
 const modeButton = document.querySelector("#mode");
 const body = document.querySelector("body");
 const main = document.querySelector("main");
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
 
-modeButton.addEventListener("click", () => {
-    body.classList.toggle('dark-mode');
-    main.classList.toggle('dark-mode');
+modeButton.addEventListener("change", () => {
+    const isDarkMode = darkModeToggle.checked;
+    body.classList.toggle('dark-mode', darkModeToggle.checked);
+    main.classList.toggle('dark-mode', darkModeToggle.checked);
+    localStorage.setItem("darkMode", darkModeToggle.checked);
 });
-
